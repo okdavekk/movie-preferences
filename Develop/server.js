28149a27a16b240d61e52a1bf67ac68da80cab37
Safwan Islam <saislam@davidson.edu>
2022-07-05 12:23:17 -0500

    added starter code

--- Develop/server.js
+++ Develop/server.js
@@ -1,17 +1,26 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
-const sequelize = require('./config/connection')
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Create the Handlebars.js engine object with custom helper functions
+const hbs = exphbs.create({ helpers });
+
+// Inform Express.js which template engine we're using
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
