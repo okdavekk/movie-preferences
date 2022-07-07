9732f4f98d5e9f0bfdb31c0b6433992bc2da9f17
James G <stackz@protonmail.com>
2022-07-07 12:07:20 -0400

    changed styling coloring made it our own

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,12 +6,12 @@ const { get_five_movies } = require("../utils/helpers.js");
 // GET all galleries for homepage
 router.get("/", async (req, res) => {
   try {
-    console.log("hello");
-    const dbGalleryData = await Preferences.findAll({});
+    // console.log("hello");
+    // const dbGalleryData = await Preferences.findAll({});
 
-    const preferences = dbGalleryData.map((preferences) =>
-      preferences.get({ plain: true })
-    );
+    // const preferences = dbGalleryData.map((preferences) =>
+    //   preferences.get({ plain: true })
+    // );
 
     const { data } = await axios.get(
       "https://api.themoviedb.org/3/trending/movie/day?api_key=9e1589a2fc403d6de0df005fb8a3d78a"
@@ -30,56 +30,56 @@ router.get("/", async (req, res) => {
 });
 
 // GET one gallery
-router.get("/gallery/:id", async (req, res) => {
-  // If the user is not logged in, redirect the user to the login page
-  if (!req.session.loggedIn) {
-    res.redirect("/login");
-  } else {
-    // If the user is logged in, allow them to view the gallery
-    try {
-      const dbGalleryData = await Gallery.findByPk(req.params.id, {
-        include: [
-          {
-            model: Painting,
-            attributes: [
-              "id",
-              "title",
-              "artist",
-              "exhibition_date",
-              "filename",
-              "description",
-            ],
-          },
-        ],
-      });
-      const gallery = dbGalleryData.get({ plain: true });
-      res.render("gallery", { gallery, loggedIn: req.session.loggedIn });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  }
-});
+// router.get("/gallery/:id", async (req, res) => {
+//   // If the user is not logged in, redirect the user to the login page
+//   if (!req.session.loggedIn) {
+//     res.redirect("/login");
+//   } else {
+//     // If the user is logged in, allow them to view the gallery
+//     try {
+//       const dbGalleryData = await Gallery.findByPk(req.params.id, {
+//         include: [
+//           {
+//             model: Painting,
+//             attributes: [
+//               "id",
+//               "title",
+//               "artist",
+//               "exhibition_date",
+//               "filename",
+//               "description",
+//             ],
+//           },
+//         ],
+//       });
+//       const gallery = dbGalleryData.get({ plain: true });
+//       res.render("gallery", { gallery, loggedIn: req.session.loggedIn });
+//     } catch (err) {
+//       console.log(err);
+//       res.status(500).json(err);
+//     }
+//   }
+// });
 
 // GET one painting
-router.get("/painting/:id", async (req, res) => {
-  // If the user is not logged in, redirect the user to the login page
-  if (!req.session.loggedIn) {
-    res.redirect("/login");
-  } else {
-    // If the user is logged in, allow them to view the painting
-    try {
-      const dbPaintingData = await Painting.findByPk(req.params.id);
+// router.get("/painting/:id", async (req, res) => {
+//   // If the user is not logged in, redirect the user to the login page
+//   if (!req.session.loggedIn) {
+//     res.redirect("/login");
+//   } else {
+//     // If the user is logged in, allow them to view the painting
+//     try {
+//       const dbPaintingData = await Painting.findByPk(req.params.id);
 
-      const painting = dbPaintingData.get({ plain: true });
+//       const painting = dbPaintingData.get({ plain: true });
 
-      res.render("painting", { painting, loggedIn: req.session.loggedIn });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  }
-});
+//       res.render("painting", { painting, loggedIn: req.session.loggedIn });
+//     } catch (err) {
+//       console.log(err);
+//       res.status(500).json(err);
+//     }
+//   }
+// });
 
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
